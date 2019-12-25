7ae1cfa42e0bed4cd584998b38aa9d4aa2479a4b
RyodoTanaka <groadpg@gmail.com>
2019-12-25 21:38:55 +0900

    Add WRS2019 projects

---
+++ WRS2019/script/TS1-DoubleArmV7S-ROS.py
@@ -0,0 +1,4 @@
+import WRSUtil
+WRSUtil.loadProject(
+    "MultiSceneViews", "TS1", "AISTSimulator", "DoubleArmV7S-TNK-ROS",
+    enableVisionSimulation = True)
---
+++ WRS2019/script/TS2-DoubleArmV7S-ROS.py
@@ -0,0 +1,4 @@
+import WRSUtil
+WRSUtil.loadProject(
+    "MultiSceneViews", "TS2", "AISTSimulator", "DoubleArmV7S-ROS",
+    enableVisionSimulation = True, remoteType = "ROS")
